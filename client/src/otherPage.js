dca1a63f005d81d617d3ef18506386f1e405edd4
Shaahin <shaahinmaesoumi@triaset.com>
2020-09-03 17:12:16 +0430

    initial commit

---
+++ client/src/otherPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-dom';
+
+
+export default () => {
+    return (
+        <div>
+            Some Other Page
+            <Link to="/">Go Back Home</Link>
+        </div>
+    );
+}
